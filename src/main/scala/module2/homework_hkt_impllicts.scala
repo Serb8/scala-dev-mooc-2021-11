bad1dcff20d2d249334f0fddf67809abbc3691b9
Serb8 <sergey.banshchykov@gmail.com>
2022-01-12 16:42:31 +0200

    changed implementation to make tupleF method to use implicit params

--- src/main/scala/module2/homework_hkt_impllicts.scala
+++ src/main/scala/module2/homework_hkt_impllicts.scala
@@ -10,24 +10,38 @@ object homework_hkt_impllicts{
       *   val r2 = println(tupleF(list1, list2))
       * 
       */
-    def tupleF[F[_], A, B](fa: Bindable[F, A], fb: Bindable[F, B]): F[(A, B)] =
-      fa.flatMap(a => fb.map(b => (a, b)))
-
+    def tupleF[F[_], A, B](fa: F[A], fb: F[B])(implicit m: Bindable[F, A], k: Bindable[F, B]): F[(A, B)] =
+        m.flatMap(fa, a => k.map(fb, b => (a, b)))
 
     trait Bindable[F[_], A] {
-        def map[B](f: A => B): F[B]
-        def flatMap[B](f: A => F[B]): F[B]
+      def map[B](c: F[A], f: A => B): F[B]
+      def flatMap[B](c: F[A], f: A => F[B]): F[B]
     }
 
-    implicit def optToBindable[A](opt: Option[A]): Bindable[Option, A] = new Bindable[Option, A] {
-      def map[B](f: A => B): Option[B] = opt.map(f)
-      def flatMap[B](f: A => Option[B]): Option[B] = opt.flatMap(f)
+    object Bindable {
+      implicit val optionInt: Bindable[Option, Int] = new Bindable[Option, Int] {
+        override def map[B](opt: Option[Int], f: Int => B): Option[B] = opt.map(f)
+        override def flatMap[B](opt: Option[Int], f: Int => Option[B]): Option[B] = opt.flatMap(f)
+      }
+
+      implicit val listInt: Bindable[List, Int] = new Bindable[List, Int] {
+        override def map[B](list: List[Int], f: Int => B): List[B] = list.map(f)
+        override def flatMap[B](list: List[Int], f: Int => List[B]): List[B] = list.flatMap(f)
+      }
     }
 
-    implicit def listToBindable[A](opt: List[A]): Bindable[List, A] = new Bindable[List, A] {
-      def map[B](f: A => B): List[B] = opt.map(f)
-      def flatMap[B](f: A => List[B]): List[B] = opt.flatMap(f)
-    }
+
+//  implicit def optToBindable[A](opt: Option[A]): Bindable[Option, A] = new Bindable[Option, A] {
+//    def map[B](f: A => B): Option[B] = opt.map(f)
+//    def flatMap[B](f: A => Option[B]): Option[B] = opt.flatMap(f)
+//  }
+//
+//  implicit def listToBindable[A](opt: List[A]): Bindable[List, A] = new Bindable[List, A] {
+//    def map[B](f: A => B): List[B] = opt.map(f)
+//    def flatMap[B](f: A => List[B]): List[B] = opt.flatMap(f)
+//  }
+
+
 
   val optA: Option[Int] = Some(1)
   val optB: Option[Int] = Some(2)
